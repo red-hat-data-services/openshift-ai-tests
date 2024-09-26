1cbdff31b0241d1709c5840e3ca21a2a1d688742
aaguirre <aaguirre@redhat.com>
2024-09-26 09:47:44 +0200

    Remove custom classes

--- tests/trustyai/conftest.py
+++ tests/trustyai/conftest.py
@@ -1,11 +1,18 @@
-from typing import Generator
+from typing import Generator, Any
 
 import pytest
 from kubernetes.dynamic import DynamicClient
 from ocp_resources.namespace import Namespace
+from ocp_resources.pod import Pod
 from ocp_resources.resource import get_client
+from ocp_resources.secret import Secret
+from ocp_resources.service import Service
 
 from tests.trustyai.utils.minio import MinioPod, MinioService, MinioSecret
+
+
+MINIO: str = "minio"
+OPENDATAHUB_IO: str = "opendatahub.io"
 
 
 @pytest.fixture(scope="session")
@@ -14,7 +21,7 @@ def admin_client() -> DynamicClient:
 
 
 @pytest.fixture(scope="class")
-def model_namespace(request, admin_client: DynamicClient) -> Namespace:
+def model_namespace(request: Any, admin_client: DynamicClient) -> Namespace:
     with Namespace(
         client=admin_client,
         name=request.param["name"],
@@ -25,19 +32,77 @@ def model_namespace(request, admin_client: DynamicClient) -> Namespace:
 
 @pytest.fixture(scope="class")
 def minio_pod(admin_client: DynamicClient, model_namespace: Namespace) -> Generator[MinioPod]:
-    with MinioPod(client=admin_client, namespace=model_namespace.name) as minio_pod:
+    with Pod(
+        name=MINIO,
+        namespace=model_namespace.name,
+        containers=[
+            {
+                "args": [
+                    "server",
+                    "/data1",
+                ],
+                "env": [
+                    {
+                        "name": "MINIO_ACCESS_KEY",
+                        "value": "THEACCESSKEY",
+                    },
+                    {
+                        "name": "MINIO_SECRET_KEY",
+                        "value": "THESECRETKEY",
+                    },
+                ],
+                "image": "quay.io/trustyai/modelmesh-minio-examples@"
+                "sha256:e8360ec33837b347c76d2ea45cd4fea0b40209f77520181b15e534b101b1f323",
+                "name": MINIO,
+            }
+        ],
+        label={"app": "minio", "maistra.io/expose-route": "true"},
+        annotations={"sidecar.istio.io/inject": "true"},
+    ) as minio_pod:
         yield minio_pod
 
 
 @pytest.fixture(scope="class")
 def minio_service(admin_client: DynamicClient, model_namespace: Namespace) -> Generator[MinioService]:
-    with MinioService(client=admin_client, namespace=model_namespace.name) as minio_service:
+    with Service(
+        name=MINIO,
+        namespace=model_namespace.name,
+        ports=[
+            {
+                "name": "minio-client-port",
+                "port": 9000,
+                "protocol": "TCP",
+                "targetPort": 9000,
+            }
+        ],
+        selector={
+            "app": "minio",
+        },
+    ) as minio_service:
         yield minio_service
 
 
 @pytest.fixture(scope="class")
 def minio_secret(admin_client: DynamicClient, model_namespace: Namespace) -> Generator[MinioSecret]:
-    with MinioSecret(client=admin_client, namespace=model_namespace.name) as minio_secret:
+    with Secret(
+        name="aws-connection-minio-data-connection",
+        namespace=model_namespace.name,
+        data_dict={
+            "AWS_ACCESS_KEY_ID": "VEhFQUNDRVNTS0VZ",
+            "AWS_DEFAULT_REGION": "dXMtc291dGg=",
+            "AWS_S3_BUCKET": "bW9kZWxtZXNoLWV4YW1wbGUtbW9kZWxz",
+            "AWS_S3_ENDPOINT": "aHR0cDovL21pbmlvOjkwMDA=",
+            "AWS_SECRET_ACCESS_KEY": "VEhFU0VDUkVUS0VZ",
+        },
+        label={
+            f"{OPENDATAHUB_IO}/dashboard": "true",
+            f"{OPENDATAHUB_IO}/managed": "true",
+        },
+        annotations={
+            f"{OPENDATAHUB_IO}/connection-type": "s3",
+            "openshift.io/display-name": "Minio Data Connection",
+        },
+    ) as minio_secret:
         yield minio_secret
 
 
